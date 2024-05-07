c43c350c28e2594037bdb1a276aa2f5a8aedcb67
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-07 19:36:12 +0700

    membuat class pelajar dan manusia

--- test.cpp
+++ test.cpp
@@ -2,7 +2,19 @@
 #include<string>
 using namespace std;
 
+class pelajar;
+class manusia{
+    public:
+    void showNilaipelajar(pelajar &x);
+};
 
+class pelajar{
+    private: int nilai;
+
+    public:
+    pelajar(){nilai = 100;}
+    friend void manusia::showNilaipelajar(pelajar &x);
+};
 
 
 
